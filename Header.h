d9d9b4d1cd268bdc93b9cb6c9b3aabeb8c6ef05c
Omar Al Sousli <osousli0@gmail.com>
2024-01-12 12:23:08 +0200

    001. Intriduction [1-8]

---
+++ Header.h
@@ -0,0 +1,9 @@
+/*  Language Basics and startup [1-8]
+    - Why to learn C++?
+    - Prerequests: Compiler and Debugger (gcc, gdb)
+    - VS code plugins: C/C++, Code Runner
+    - C++ code compiling: 
+        Preprocessing > Source code > Translation Unit > Object file > linking > EXE file
+    - Syntax
+    - Comments
+ */
